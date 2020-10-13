264b7a6a11cb453c407c67bf45e207f1ddf2b158
aet37 <aet37@pitt.edu>
2020-10-12 22:37:02 -0400

    Update RpiGPIOTest.py

--- RpiGPIOTest.py
+++ RpiGPIOTest.py
@@ -6,7 +6,7 @@ GPIO.setmode(GPIO.BOARD)
 GPIO.setup(32, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)	# Set as internal pull up resistor input
 
 while True:
-	GPIO.wait_for_edge(32, GPIO.FALLING, timeout=195)
+	GPIO.wait_for_edge(32, GPIO.RISING, timeout=195)
 
 	time.sleep(0.05) # debounce
 
