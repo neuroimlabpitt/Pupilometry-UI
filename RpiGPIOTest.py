38335114c65a9942f92fa8fd8be2348fd236e80b
aet37 <aet37@pitt.edu>
2020-10-12 22:34:50 -0400

    Update RpiGPIOTest.py

--- RpiGPIOTest.py
+++ RpiGPIOTest.py
@@ -1,21 +1,22 @@
 import RPi.GPIO as GPIO
 import time
 
-'''
+
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(40, GPIO.IN, pull_up_down=GPIO.PUD_UP)	# Set as internal pull up resistor input
+GPIO.setup(32, GPIO.IN, pull_up_down=GPIO.PUD_UP)	# Set as internal pull up resistor input
 
 while True:
-	GPIO.wait_for_edge(40, GPIO.FALLING, timeout=195)
+	GPIO.wait_for_edge(32, GPIO.FALLING, timeout=195)
 
 	time.sleep(0.05) # debounce
 
-	if GPIO.input(40) == 0:
+	if GPIO.input(32) == 0:
 		print("Button Pressed!!")
 	else:
 		time.sleep(0.195)
-'''
 
+
+'''
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(32, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
@@ -27,3 +28,4 @@ while True:
 		print("Button Not Pressed")
 	if(GPIO.input(32) == 0):
 		print("Button Pressed")
+'''
