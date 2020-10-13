e363bcec96b16cf54a06ec8afa3769297fd6ffbb
aet37 <aet37@pitt.edu>
2020-10-12 22:07:58 -0400

    test button press

---
+++ RpiGPIOTest.py
@@ -0,0 +1,16 @@
+import RPi.GPIO as GPIO
+import time
+
+
+GPIO.setmode(GPIO.board)
+GPIO.setup(40, GPIO.IN, pull_up_down=GPIO.PUD_UP)	# Set as internal pull up resistor input
+
+while True:
+	GPIO.wait_for_edge(40, GPIO.FALLING, timeout=195)
+
+	time.sleep(0.05) # debounce
+
+	if GPIO.input(40) == 0:
+		print("Button Pressed!!")
+	else:
+		time.sleep(0.195)
