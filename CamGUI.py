90af54e39aabd8d62d5548682a7b106592481baf
aet37 <aet37@pitt.edu>
2020-10-15 15:09:16 -0400

    Update CamGUI.py

--- CamGUI.py
+++ CamGUI.py
@@ -376,6 +376,7 @@ class CamGUI:
         while True:
             GPIO.wait_for_edge(args.trigger_pin, GPIO.RISING, timeout=195)
             print("Trigger Recived")
+            break
             #time.sleep(0.005) #debounce 5ms
         '''
         # double-check - workaround for messy edge detection
