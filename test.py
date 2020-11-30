882af5c5f4450a21bf4748e48a4cb0c683f42e7f
aet37 <aet37@pitt.edu>
2020-11-30 14:13:00 -0500

    Update test.py

--- test.py
+++ test.py
@@ -6,10 +6,16 @@ import picamera.array
 
 # Capture image
 print("Capturing image...")
+
+start_time = time.time()
+
 with picamera.PiCamera() as camera:
     with picamera.array.PiBayerArray(camera) as stream:
         camera.capture(stream, 'jpeg', bayer=True)
         # Demosaic data and write to rawimg
         rawimg = stream.demosaic()
 
+execution_time = time.time() - start_time
+
+print('Time is: ', execution_time)
 print(rawimg.size)
