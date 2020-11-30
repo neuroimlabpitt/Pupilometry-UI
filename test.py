749b6169e54cf7c234ffb761c5feba5f857ab6ea
aet37 <aet37@pitt.edu>
2020-11-30 14:10:45 -0500

    Update test.py

--- test.py
+++ test.py
@@ -4,3 +4,12 @@ import numpy as np
 import picamera
 import picamera.array
 
+# Capture image
+print("Capturing image...")
+with picamera.PiCamera() as camera:
+    with picamera.array.PiBayerArray(camera) as stream:
+        camera.capture(stream, 'jpeg', bayer=True)
+        # Demosaic data and write to rawimg
+        rawimg = stream.demosaic()
+
+print(rawimg.size)
