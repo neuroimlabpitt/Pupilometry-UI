1074843263ea5660d4451df4a1b5e43d08586843
aet37 <aet37@pitt.edu>
2020-11-30 14:31:25 -0500

    Update test.py

--- test.py
+++ test.py
@@ -1,9 +1,11 @@
 import time
 import os
+import io
 import numpy as np
 import picamera
 import picamera.array
 
+'''
 # Capture image
 print("Capturing image...")
 
@@ -19,3 +21,18 @@ execution_time = time.time() - start_time
 
 print('Time is: ', execution_time)
 print(rawimg.size)
+'''
+
+# Capture image
+print("Capturing image...")
+
+start_time = time.time()
+
+stream = io.BytesIO()
+
+with picamera.PiCamera() as camera:
+    camera.capture(stream, 'jpeg', bayer=True)
+
+execution_time = time.time() - start_time
+
+print('Time is: ', execution_time)
